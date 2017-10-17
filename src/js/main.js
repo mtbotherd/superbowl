9814b8ac8297267cf5b3b60f27829af58f5be668
mtbotherd <mtbotherd@users.noreply.github.com>
2017-10-17 15:44:13 -0500

    latest progress.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
-    // $('.park-ride').on('click', function() {
-    //     $('.park-ride').popover('show');
-    // });
-
-    $('[data-toggle="popover"]').popover();
-    $('[data-toggle="popoever"]').popover('show');
+    $(document).on('click', '.navbar-collapse.in', function(e) {
+        if ($(e.target).is('a')) {
+            $(this).collapse('hide');
+        }
+    });
 });
