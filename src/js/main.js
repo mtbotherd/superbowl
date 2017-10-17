d272d713ecd569ef64dec5bcaa5b615be5c39035
mtbotherd <mtbotherd@users.noreply.github.com>
2017-10-17 08:51:10 -0500

    Progress

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    // $('.park-ride').on('click', function() {
+    //     $('.park-ride').popover('show');
+    // });
+
+    $('[data-toggle="popover"]').popover();
+    $('[data-toggle="popoever"]').popover('show');
+});
