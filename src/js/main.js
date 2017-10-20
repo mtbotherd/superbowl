8b4ebb0c8dc35db795806ee1688bcf20e04e6e92
mtbotherd <mtbotherd@users.noreply.github.com>
2017-10-20 13:53:33 -0500

    Fixing scrolling for mobile menu.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+    // prevent body scroll when menu is open
+    $(function() {
+
+        var fixed = document.getElementById('fixed'),
+            overflow;
+
+        $(window).on('load resize', function() {
+            overflow = fixed.scrollHeight - $('#fixed').height();
+        });
+
+        fixed.on('touchmove', function() {
+
+            if (overflow) return true;
+            else return false;
+        });
+    });
+
     // Collapse menu when link it clicked (mobile only).
     $(document).on('click', '.navbar-collapse.in', function(e) {
         if ($(e.target).is('a')) {
