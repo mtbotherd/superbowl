adfbe91621aead6cac483321b0fc738f13d0bb55
mtbotherd <mtbotherd@users.noreply.github.com>
2017-10-24 14:26:41 -0500

    remove body scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-
-    // prevent body scroll when menu is open
-    $('body').bind('touchmove', function(e) {
-        e.preventDefault()
-    });
-    $('body').unbind('touchmove');
-
     // Collapse menu when link it clicked (mobile only).
     $(document).on('click', '.navbar-collapse.in', function(e) {
         if ($(e.target).is('a')) {
