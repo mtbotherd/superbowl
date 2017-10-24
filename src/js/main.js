1504dff2f15034aeeabfcdc5786ef850bcd8c6a6
mtbotherd <mtbotherd@users.noreply.github.com>
2017-10-24 14:23:43 -0500

    Fixed accordion.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
-    // prevent body scroll when menu is open
-    $('body').bind('touchmove', function(e) {
-        e.preventDefault()
+    $(document).on('show', '#accordion', function(e) {
+        //$('.accordion-heading i').toggleClass(' ');
+        $(e.target).prev('.accordion-heading').addClass('accordion-opened');
     });
-    $('body').unbind('touchmove');
+
+    $(document).on('hide', '#accordion', function(e) {
+        $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+        //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+    });
 
     // Collapse menu when link it clicked (mobile only).
     $(document).on('click', '.navbar-collapse.in', function(e) {
